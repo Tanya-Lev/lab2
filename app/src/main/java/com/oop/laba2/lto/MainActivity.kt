b1b6de7997e200d421ad9fe2f2184649eeaa22e8
1 <ceeper.ru@mail.ru>
2022-05-13 06:21:31 +0300

    navigation bar не работал, тк надо было УБРАТЬ ИЗ ТЕМЫ NoActionBar

--- app/src/main/java/com/oop/laba2/lto/MainActivity.kt
+++ app/src/main/java/com/oop/laba2/lto/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import androidx.core.graphics.toColorFilter
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -27,9 +28,11 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
             R.id.navigation_selections))
